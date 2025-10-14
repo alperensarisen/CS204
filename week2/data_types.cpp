7303acc5617b673a32ff38c640f9b396ab406e60
apoliko <64897230+apoliko@users.noreply.github.com>
2025-10-14 20:26:46 +0300

    week 2 data_types
    This lecture was abaout datas and their types in the memory

---
+++ week2/data_types.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //all data in the computer's memory is represented as a squaence of bits
+    //Bit: unit of storage. Can be either 0 or 1.
+    //Byte: 8 bits make up a byte, and a character is one byte.
+    //A byte can represent 256 (0-255) different symbols.
+
+    // 0 0 0 0 0 0 1 0 = 2
+    // 1 1 1 1 1 1 1 1 = 255
+
+    // Word: typical data size used in computer
+    //    -32 bits on some old computers
+    //    -64 bits on most current computers
+
+    // HEX Numbers:
+    //Similar to binary coding
+    //Each digit can take values: 0,1,2,3,4,5,6,,7,8,9,A,B,C,D,E,F
+    //0-9, A = 10, B = 11 ... F = 15
+    // As an example:
+    //2435af00 or 0x2435af00 bit-byte representation is
+    //0010|0100|0011|0101|1010|1110|0000|0000
+    // When we define a variable it requires a memory
+    
+    return 0;
+}
