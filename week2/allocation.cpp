a3b42507341b1a8e8f841d2897f5312da3cbb1a4
apoliko <64897230+apoliko@users.noreply.github.com>
2025-10-15 03:11:57 +0300

    part2 of memory allocation
    I added some new information about pointers and their usage.

--- week2/allocation.cpp
+++ week2/allocation.cpp
@@ -10,6 +10,7 @@ class Person{ //very basic person class for practice
     //constructor
         Person(string n, string s, int a)
         : name(n),surname(s),age(a){}
+        Person() : name(""), surname(""), age(0) {}
     void printInfo(){
         cout << "Name: " << name << endl;
         cout << "Surname: " << surname << endl;
@@ -21,9 +22,28 @@ int main(){
     p1.printInfo();
     Person *p_Person; //this is a class type pointer
     p_Person = &p1; //pointer points to address of p1 object
-    cout <<"The address of p1 is "<<p_Person<<endl;
+    cout <<"The address of p1 is: "<<p_Person<<endl;
     cout <<p_Person->age<<endl;
     // ptr->is a shorthand for (*ptr).
-   
+    Person *P1p = new Person;
+    P1p = &p1;
+    cout<<"The age of p1 object is: " <<P1p->age<<endl;
+    //
+    //int *p_1, *p_2;
+    //p_1 = new int;      // it allocates enough memory for a single int
+    //p_2 = new int[4];   //it allocates memory for 4 integers.
+    int *primenumbers = new int[100];
+    primenumbers[0] = 2;
+    primenumbers[1] = 3;
+    cout << "First prime is " << primenumbers[0]<<endl;
+    // primenumbers[0] and *primenumbers are the same thing
+    cout<<*(primenumbers+1)<<" is equal to "<<primenumbers[1]<<endl;
+    //In general, ptr+x means, the xth memory location (of the type of ptr) in the memory after ptr.
+
+    int myArray[100];
+    int *pInt = &(myArray[5]);
+    int *pInt2 = &(myArray[10]);
+    cout<<pInt<<"  "<<pInt2<<endl;
+    cout<<pInt2-pInt<<endl;
     return 0;
 }
