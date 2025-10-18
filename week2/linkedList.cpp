5016a695321fe1fc935c24ed7f8be4b4b763c002
Alperen Sarışen <alperensarisen2@gmail.com>
2025-10-18 03:20:20 +0300

    Week 2 Linked list basics
    This lecture was about basics of linked lists and basic syntax.

---
+++ week2/linkedList.cpp
@@ -0,0 +1,41 @@
+    //! LINKED LIST
+    //? dynamic memory allocation
+    //? insertion/deletion is cheap (no shifting)
+    //? more cumbersome to program with
+#include <iostream>
+#include <string>
+using namespace std;
+struct node {
+    //string word;
+    //int num;
+    
+    int data;
+    node *next;  /* pointer for the next node */ 
+    
+    //default constructor
+    node(){}
+    //constructor
+    //node(string w, int n):word(w),num(n){}
+};
+int main(){
+    node *head = new node();
+    node* second = new node();
+    node* third = new node();
+
+    head->data=10;
+    head->next = second;
+    
+    second->data=20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = nullptr;
+
+    node *temp = head;
+    while(temp != nullptr){
+        cout<<temp->data<<endl;
+        temp = temp->next;
+    }
+    //! [10] -> [20] -> [30] -> NULL
+    return 0;
+}
