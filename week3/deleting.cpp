504e7c89217ca63d928d01f95258bfc88482ecc3
apoliko <64897230+apoliko@users.noreply.github.com>
2025-10-20 23:09:47 +0300

    basic instertion

--- week3/deleting.cpp
+++ week3/deleting.cpp
@@ -13,20 +13,48 @@ node* add2end(node* tail, int id){
     tail->next = nn;
     return nn;
 }   
-void DeleteList(node* head){
+void DeleteList(node*& head){
     node* temp;
     while(head != nullptr){
-        temp = head;
+        temp = head->next;
         delete head;
         head = temp;
     }
 }
 void printLL(node * temp){
+    
     while(temp != nullptr){
-        cout<<temp->id<<" ";
-        temp = temp->next;
+    cout<<temp->id<<" ";
+    temp = temp->next;
     }
-
+    cout<<endl;
+}
+//! Recursion version of deleting
+void DeleteLL(node*& head){
+    if(head){
+        DeleteLL(head->next);
+        delete head;
+        head = nullptr;
+    }
+}
+node * AddInOrder(node* head, int newkey){
+    node* ptr = head; //loop variable
+    if(!head||newkey<head->id){
+        node* temp = new node;
+        temp->id = newkey;
+        temp->next = head;
+        return temp;
+    }
+    //! To chechk node one ahead so we dont pass!
+    while(ptr->next && ptr->next->id < newkey){
+        ptr = ptr->next;
+    }
+    // postcondition: new node to be inserted just after the node ptr points
+    node* temp = new node; //!node to be inserted
+    temp->id = newkey;
+    temp->next = ptr->next; //! connect to rest
+    ptr->next = temp;
+    return head;
 }
 int main(){
     node *head = new node(nullptr,10);
@@ -38,7 +66,9 @@ int main(){
     tail = add2end(tail,50);
     //! Until there I just created basic Linked List to use.
     printLL(head);
-    DeleteList(head);
+    //DeleteList(head);
+    //DeleteLL(head);
+    node * p = AddInOrder(head,25);
     printLL(head);
     return 0;
 }
