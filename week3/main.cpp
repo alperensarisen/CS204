73619d7bdda91ce7b3708bf63f93561875e6bba8
apoliko <64897230+apoliko@users.noreply.github.com>
2025-10-22 23:43:11 +0300

    new type array and delete

--- week3/main.cpp
+++ week3/main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 int main(){
     linkedlist list;
     //!TYPEDEF
+    typedef int* IntPtr;
+    int a = 10;
+    IntPtr p = &a;
+//! Avoids mistake of forgetting the  * character
+//! Also you can use reference parameters instead of pointer
+//! void swap (int & x, int & y)
+//! void swap (int * x, int * y)
+//! both of them are able to used.
     list.addToBeginning(40);
     list.addToBeginning(30);
     list.addToBeginning(20);
