0af7c68aee415a3852fd21e5378d81702c31114f
apoliko <64897230+apoliko@users.noreply.github.com>
2025-10-25 16:39:01 +0300

    arrays type and delete

---
+++ newArrays.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+//! 1D arrays with new operator
+//! To dynamically allocate an array, specify the array size in
+//! square brackets [] after the type:
+    typedef int *ptrInt;
+    ptrInt p;
+    p = new int[20];
+    return 0;
+}
