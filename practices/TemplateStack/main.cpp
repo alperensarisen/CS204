992232381cf896d0ed5b505442540f06a75a154d
apoliko <64897230+apoliko@users.noreply.github.com>
2025-11-20 03:09:42 +0300

    added operators and copy constructor etc.

--- practices/TemplateStack/main.cpp
+++ practices/TemplateStack/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <iosfwd>
 using namespace std;
 template <class itemType>
 struct node{
@@ -12,17 +14,24 @@ class Stack{
         node<itemType> *head;
         int size;
     public:
-        Stack();        //TODO: copy constructor!
+        Stack();        
+        Stack(const Stack<itemType> & other);  //? copy constructor!
         ~Stack();
         void add(itemType n);
         void pop();
+        void copyFrom(const Stack<itemType> & other);
         itemType & top() const;
         bool isEmpty() const;
         int getSize() const;
         void clear();
+        void printList() const;
+        Stack<itemType> & operator =(const Stack<itemType> & rhs);
+        friend ostream& operator<<(ostream & out, const Stack<itemType>& stack);
 };
 template<class itemType>
 Stack<itemType>::Stack():head(nullptr),size(0){}
+template<class itemType>
+Stack<itemType>::Stack(const Stack<itemType> & other){copyFrom(other);}
 template<class itemType>
 Stack<itemType>::~Stack(){clear();}
 template<class itemType>
@@ -45,6 +54,23 @@ void Stack<itemType>::pop(){
     head = head->next;
     delete p;
     size--;
+}
+template<class itemType>
+void Stack<itemType>::copyFrom(const Stack<itemType> & other){
+    head = nullptr;
+    size = 0;
+    if(!other.head) return;
+    head = new node<itemType>(other.head->value,nullptr);
+    size = 1;
+
+    node<itemType> *nh = head;
+    node<itemType> *p = other.head->next;
+    while(p){
+        nh->next = new node<itemType>(p->value,nullptr);
+        nh = nh->next;
+        p = p->next;
+        size++;
+    }
 }
 template<class itemType>
 itemType & Stack<itemType>::top() const{
@@ -70,78 +96,81 @@ void Stack<itemType>::clear(){
     }
     size = 0;
 }
+template<class itemType>
+void Stack<itemType>::printList() const{
+    if(!head){
+        cout<<"Empty Stack! p:\n";
+        return;
+    }
+    node<itemType> *temp = head;
+    while(temp){
+        cout<<temp->value<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+template<class itemType>
+Stack<itemType> & Stack<itemType>::operator=(const Stack<itemType> & rhs){
+    if(this == &rhs) return *this;
+    clear();
+    copyFrom(rhs);
+    return *this;
+}
+template<class itemType>        
+ostream& operator<<(ostream &out,const Stack<itemType> stack){  //TODO: SOLVE THIS PROBLEM
+    node<itemType> *p = stack.head;
+    if(!p) return out<<"EMPTY STACK!\n";
+    while(p){
+        out<<p->value<<"\n";
+        p = p->next;
+    }
+    return out;
+}
 int main() {
-    cout << "====== STACK TEST ======\n\n";
-
-    // ----------------------------
-    // 1) INT STACK TEST
-    // ----------------------------
-    cout << "INT STACK TEST\n";
-
+    // ===== INT STACK =====
     Stack<int> s;
 
-    cout << "Is empty? " << (s.isEmpty() ? "YES" : "NO") << endl;
-
-    cout << "Adding 10, 20, 30...\n";
     s.add(10);
     s.add(20);
     s.add(30);
 
-    cout << "Size: " << s.getSize() << endl;
-    cout << "Top: " << s.top() << endl;      // Expected: 30
+    cout << "Int stack size: " << s.getSize() << "\n";
+    cout << "Top: " << s.top() << "\n";   // 30
 
-    cout << "Pop...\n";
     s.pop();
-    cout << "New top: " << s.top() << endl;  // Expected: 20
-    cout << "Size: " << s.getSize() << endl;
+    cout << "Top after pop: " << s.top() << "\n"; // 20
 
-    // ----------------------------
-    // 2) POP UNTIL EMPTY
-    // ----------------------------
-    cout << "\nPOP ALL ELEMENTS:\n";
-    while(!s.isEmpty()){
-        cout << "Popping: " << s.top() << endl;
-        s.pop();
-    }
+    // Hepsini boşalt
+    s.clear();
+    cout << "Is int stack empty? " << (s.isEmpty() ? "YES" : "NO") << "\n\n";
 
-    cout << "Is empty now? " << (s.isEmpty() ? "YES" : "NO") << endl;
-
-    // ----------------------------
-    // 3) POP FROM EMPTY STACK (Exception)
-    // ----------------------------
-    cout << "\nTEST EXCEPTION ON POP (EXPECTED)\n";
-    try {
-        s.pop();
-    }
-    catch (exception& e) {
-        cout << "Caught exception: " << e.what() << endl;
-    }
-
-    // ----------------------------
-    // 4) STRING STACK TEST
-    // ----------------------------
-    cout << "\nSTRING STACK TEST\n";
-
+    // ===== STRING STACK =====
     Stack<string> st;
-    st.add("Gamze");
-    st.add("Ablam");
-    st.add("Canim");
+    st.add("Jack");
+    st.add("Ronny");
+    st.add("Jahson");
 
-    cout << "Size: " << st.getSize() << endl;
-    cout << "Top: " << st.top() << endl;     // Canim
+    cout << "String stack size: " << st.getSize() << "\n";
+    cout << "Top: " << st.top() << "\n";   // Jahson
 
     st.pop();
-    cout << "Top after one pop: " << st.top() << endl;   // Ablam
+    cout << "Top after pop: " << st.top() << "\n";   // Ronny
 
-    // ----------------------------
-    // 5) CLEAR TEST
-    // ----------------------------
-    cout << "\nCLEAR TEST\n";
-    
     st.clear();
-    cout << "After clear, size = " << st.getSize() << endl;
-    cout << "Is empty? " << (st.isEmpty() ? "YES" : "NO") << endl;
+    cout << "After clear, empty? " << (st.isEmpty() ? "YES" : "NO") << "\n";
 
     cout << "\n====== END OF TEST ======\n";
+
+    Stack<string> stringStack;
+    stringStack.add("hello");
+    stringStack.add("cpp");
+    stringStack.add("world");
+    cout<<"LIST 1: \n";
+    stringStack.printList();
+    Stack<string> stringStack2;
+    stringStack2 = stringStack;
+    cout<<"LIST 2:\n";
+    stringStack2.printList();
+    cout<<stringStack;//TODO: solve the problem in here
     return 0;
 }
