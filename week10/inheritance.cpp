595444ade85bd5ac69d674df42fad91953b90206
apoliko <64897230+apoliko@users.noreply.github.com>
2025-12-17 23:12:07 +0300

    types of subclass deriving  and features

--- week10/inheritance.cpp
+++ week10/inheritance.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string> 
 using namespace std;
-class Pet{
+class Pet{  //Supperclass(base class)
     public:
         Pet():weight(1),food("Pet Chow"){}
         ~Pet(){}
@@ -22,13 +22,14 @@ void Pet::eat(){cout<<"eating food: "<<food<<endl;}
 void Pet::speak(){cout<<"Growl"<<endl;}
 
 //! Declaring a new subclass:
-class Rat: public Pet{  //Other methods can be added
+class Rat: public Pet{  //Subclass (derived class)
+    //Other methods can be added
     public: 
         Rat(){}
         ~Rat(){}
         void sicken(){cout <<"Spreading Plague"<<endl;}
 };
-class Cat: public Pet{
+class Cat: public Pet{  //Subclass (derived class)
     public:
         Cat(){}
         ~Cat(){}
@@ -48,3 +49,17 @@ int main(){
     fluffy.eat();
     return 0;
 }
+/*
+?   You can derive a subclass in 3 different ways:
+*       class Rat: public Pet
+&           public: as usual, any class/function can access these variables/methods and 
+&           subclass also inherit and access  them
+*       class Rat: public Pet
+&           private: as usual, no other class/function can access these variables and 
+&           subclasses inherit but CANNOT access these members
+*       class Rat: protected Pet
+&           protected: no other classes/functions can access them, but subclasses 
+&           inherit and accessthem (so it is a relaxed form of private)     
+
+
+*/
