31132df2edb9487dd3b87128d1948f70187d7691
apoliko <64897230+apoliko@users.noreply.github.com>
2026-01-09 01:24:01 +0300

    join and detach logic

---
+++ week12/multithreading.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std;
+/*void hello(){
+    //chrono::seconds dura(2);
+    //this_thread::sleep_for(dura);
+    cout<<"Hello thread\n";
+}
+int main(){
+    thread athread(&hello);
+    athread.detach();
+    cout<<"Bye main\n";
+    if(athread.joinable()){cout<<"athread is joinable\n";}
+    else cout<<"athread is not joinable\n";
+    return 0;
+}*/
+void pause_thread(int n){
+    this_thread::sleep_for(chrono::seconds(n));
+    cout<<"thread "<<n<<" joined\n";
+}
+int main(){
+    cout<<"Spawning 2 threads...\n";
+    thread t1(pause_thread,1);  //function, parameter 1, parameter 2, ...
+    thread t2(pause_thread,2);
+    cout<<"Done spawning threads.\n";
+    cout<<"Now waiting for them to join...\n";
+    t1.join();
+    t2.join();
+    cout<<"All threads joined\n";
+    return 0;
+}
